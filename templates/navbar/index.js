2533a5e2a353f12e2568ef494af506975a8e4893
allenyll <806141743@qq.com>
2020-12-03 16:59:10 +0800

    优化订单页面

---
+++ templates/navbar/index.js
@@ -0,0 +1,55 @@
+// components/navbar/index.js
+const app = getApp();
+
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    pageName: String,
+    bgColor: String,
+    iconColor: String,
+    fontColor: String,
+    showNav: Boolean,
+    showHome: {
+      type: Boolean,
+      value: true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+   
+  },
+  lifetimes: {
+    attached: function () {
+      this.setData({
+        navHeight: app.globalData.navHeight,
+        navTop: app.globalData.navTop,
+        windowHeight: app.globalData.windowHeight
+      })
+     }
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    //回退
+    navBack: function () {
+        wx.navigateBack({
+          delta: 1
+        })      
+    },
+    //回主页
+    toIndex: function () {
+      wx.navigateTo({
+        url: '/pages/admin/home/index/index'
+      })
+    },
+  }
+})
